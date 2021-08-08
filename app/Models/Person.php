face4d34151cacc5d72a19c3082e312f2fe30384
Laurane <laurane.mh@gmail.com>
2021-08-08 10:52:42 +0200

    Add config models

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,10 +2,60 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Person
+ *
+ * @property $id
+ * @property $name
+ * @property $first_name
+ * @property $username
+ * @property $password
+ * @property $role_id
+ *
+ * @property MaterialPerson[] $materialPeoples
+ * @property Role $role
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Person extends Model
 {
-    use HasFactory;
+    public $timestamps = false;
+
+    static $rules = [
+        'name' => 'required',
+        'first_name' => 'required',
+        'username' => 'required',
+        'password' => 'required',
+        'role_id' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','first_name','username','password','role_id'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function materialPeoples()
+    {
+        return $this->hasMany('App\Models\MaterialPerson', 'person_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function role()
+    {
+        return $this->hasOne('App\Models\Role', 'id', 'role_id');
+    }
+
+
 }
